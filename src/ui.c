c3c4c98aea443dc105b34d4e8e3aebc885734971
gonmf <gonmf@sapo.pt>
2021-12-01 21:28:06 +0000

    Improve map

--- src/ui.c
+++ src/ui.c
@@ -1,15 +1,17 @@
 #include "global.h"
 
-static void paint_part(pixel_t color, unsigned int x, unsigned int y, unsigned int offset_x, unsigned int offset_y) {
-    for (unsigned int by = 0; by < MAP_BLOCK_SIZE; ++by) {
-        unsigned int y2 = y * MAP_BLOCK_SIZE + by + offset_y;
+static void paint_part(pixel_t color, unsigned int x, unsigned int y, unsigned int offset_x, unsigned int offset_y, bool small) {
+    unsigned int shift = small ? MAP_BLOCK_SIZE / 5 : 0;
+
+    for (unsigned int by = 0; by < MAP_BLOCK_SIZE - shift * 2; ++by) {
+        unsigned int y2 = y * MAP_BLOCK_SIZE + by + offset_y + shift;
 
         if (y2 >= VIEWPORT_HEIGHT) {
             return;
         }
 
-        for (unsigned int bx = 0; bx < MAP_BLOCK_SIZE; ++bx) {
-            unsigned int x2 = x * MAP_BLOCK_SIZE + bx + offset_x;
+        for (unsigned int bx = 0; bx < MAP_BLOCK_SIZE - shift * 2; ++bx) {
+            unsigned int x2 = x * MAP_BLOCK_SIZE + bx + offset_x + shift;
 
             if (x2 >= VIEWPORT_WIDTH) {
                 return;
@@ -36,7 +38,7 @@ void paint_map(const level_t * level) {
         unsigned int iy = level->height - y - 1;
         for (unsigned int x = 0; x < level->width; ++x) {
             if (x == observer_x && iy == observer_y) {
-                paint_part(color_red, x, y, offset_x, offset_y);
+                paint_part(color_red, x, y, offset_x, offset_y, true);
             } else {
                 bool revealed = level->map_revealed[x + iy * level->width];
                 unsigned char content_type = level->content_type[x + iy * level->width];
@@ -46,20 +48,22 @@ void paint_map(const level_t * level) {
                 }
 
                 if (content_type == CONTENT_TYPE_WALL) {
-                    paint_part(color_gray, x, y, offset_x, offset_y);
+                    paint_part(color_gray, x, y, offset_x, offset_y, false);
                 } else if (content_type == CONTENT_TYPE_DOOR || content_type == CONTENT_TYPE_DOOR_OPEN) {
-                    paint_part(color_cyan, x, y, offset_x, offset_y);
-                /*
-                TODO: paint blocking objects
-                } else if (content_type == CONTENT_TYPE_OBJECT) {
-                    if (level->special_effects[x + iy * level->width] == SPECIAL_EFFECT_NONE) {
-                        paint_part(color_gray, x, y, offset_x, offset_y);
-                    } else {
-                        paint_part(color_gold, x, y, offset_x, offset_y);
-                    }
-                */
+                    paint_part(color_cyan, x, y, offset_x, offset_y, false);
                 }
             }
+        }
+    }
+
+    for (unsigned int i = 0; i < level->objects_count; ++i) {
+        unsigned int x = (unsigned int)level->object[i].x;
+        unsigned int y = level->height - ((unsigned int)level->object[i].y) - 1;
+
+        if (level->object[i].type == OBJECT_TYPE_BLOCKING) {
+            paint_part(color_gray, x, y, offset_x, offset_y, true);
+        } else {
+            paint_part(color_gold, x, y, offset_x, offset_y, true);
         }
     }
 }
